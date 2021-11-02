Added 180 rotation and Fixed FindSolutionQuickVersion bugs
Hard Drop should be the last move, it has a confirmation effect.

180 rotation is now available!

-  Added few other test files
- Added SOFT_DROP type.

agents/agent_test.py
@@ -13,6 +13,18 @@ class AgentTest(unittest.TestCase):
                          restart=self.game.Restart)
     self.agent = agent.Agent(env=self.env)
 
+  def _CheckPaths(self, rst):
+    for(s, ps) in rst:
+      self._CheckPath(s, ps)
+
+  def _CheckPath(self, final_state, path):
+    # Checks the paths
+    if path[0].swap:
+      return
+    game = self.game.copy()
+    game.ProcessActions(path, post_processing=False)
+    self.assertEqual(final_state, game.current_piece)
+
   def test_GetAllPossiblePositions(self):
     t = shape.T()
     t.y = 0
@@ -119,13 +131,9 @@ class AgentTest(unittest.TestCase):
     rst_set = set([(p.x, p.y, p.state) for (p, path) in rst])
     self.assertIn((19, 1, 2), rst_set)
 
-  def test_GetAllPossiblePositions_Quick(self):
+  def test_GetAllPossiblePositions_Quick_T(self):
     t = shape.T(start_x=0, start_y=0)
-    self.game = game_client.GameClient()
-    self.game.SpawnPiece(t)
-
-    self.game.width = 5
-    self.game.height = 6
+    self.game = game_client.GameClient(width=5, height=6, height_buffer=0)
     self.game.map = np.array(
       [# 0  1  2  3  4
         [0, 0, 0, 0, 0, ],  # 0
@@ -137,15 +145,92 @@ class AgentTest(unittest.TestCase):
       ]
     )
 
+    self.game.SpawnPiece(t)
     rst = agent.GetPossiblePositionsQuickVersion(t, self.game)
 
+    # Checks the final states
     # The quick solution is suppose to at least get 4 T-spin rotations.
-    self.assertGreater(len(rst), 4)
+    self.assertEqual(len(rst), 18)
 
     # And the T-Spin double should at least be in the ans.
     rst_set = set([(p.x, p.y, p.state) for (p, path) in rst])
     self.assertIn((3, 1, 2), rst_set)
 
+    # Checks the paths
+    self._CheckPaths(rst)
+
+  def test_GetAllPossiblePositions_Quick_O(self):
+    o = shape.O(start_x=0, start_y=0)
+    self.game = game_client.GameClient(width=5, height=6, height_buffer=0)
+    self.game.map = np.array(
+      [  # 0  1  2  3  4
+        [0, 0, 0, 0, 0, ],  # 0
+        [0, 0, 0, 0, 0, ],  # 1
+        [0, 0, 0, 0, 0, ],  # 2
+        [0, 0, 0, 0, 0, ],  # 3
+        [0, 0, 0, 0, 0, ],  # 4
+        [0, 0, 0, 0, 0, ],  # 5
+      ]
+    )
+
+    self.game.SpawnPiece(o)
+
+    rst = agent.GetPossiblePositionsQuickVersion(o, self.game)
+
+    # Since this is an O shape, there are only 4 + 1(swap) possible solutions.
+    self.assertEqual(len(rst), 5)
+
+    self._CheckPaths(rst)
+
+  def test_GetAllPossiblePositions_Quick_Z(self):
+      z = shape.Z(start_x=0, start_y=0)
+      self.game = game_client.GameClient(width=5, height= 6, height_buffer=0)
+      self.game.SpawnPiece(z)
+
+      self.game.map = np.array(
+        [  # 0  1  2  3  4
+          [0, 0, 0, 0, 0, ],  # 0
+          [0, 0, 0, 0, 0, ],  # 1
+          [0, 0, 0, 0, 0, ],  # 2
+          [6, 0, 0, 0, 0, ],  # 3
+          [6, 6, 2, 0, 0, ],  # 4
+          [6, 6, 6, 3, 3, ],  # 5
+        ]
+      )
+
+      rst = agent.GetPossiblePositionsQuickVersion(z, self.game)
+
+      self._CheckPaths(rst)
+
+  def test_GetAllPossiblePositions_Quick_RotSlideRotSlide(self):
+    """Tests rotation -> slide -> rotation -> slide."""
+
+    i = shape.I(start_x=0, start_y=0)
+    self.game = game_client.GameClient()
+    self.game.SpawnPiece(i)
+
+    self.game.width = 5
+    self.game.height = 6
+    self.game.map = np.array(
+      [  # 0  1  2  3  4
+        [0, 0, 0, 0, 0, ],  # 0
+        [0, 0, 0, 0, 0, ],  # 1
+        [0, 0, 0, 0, 0, ],  # 2
+        [0, 0, 0, 0, 0, ],  # 3
+        [0, 0, 0, 0, 9, ],  # 4
+        [0, 0, 0, 0, 0, ],  # 5
+      ]
+    )
+
+    rst = agent.GetPossiblePositionsQuickVersion(i, self.game)
+    self._CheckPaths(rst)
+
+    for (s, _) in rst:
+      self.game.SpawnPiece(s)
+      self.game.TextDraw()
+
+    rst_set = set([(p.x, p.y, p.state) for (p, path) in rst])
+    self.assertIn((4, 1, 0), rst_set)
 
 if __name__ == '__main__':
   unittest.main()

agents/near_perfect_bot.py
@@ -9,6 +9,8 @@ from typing import List, Tuple
 import actions
 import numpy as np
 
+class InternalException(Exception):
+  """Raises upon any errors."""
 
 class TheNearPerfectAgent(agent.Agent):
   def __init__(self, env: agent.Env,
@@ -19,33 +21,36 @@ class TheNearPerfectAgent(agent.Agent):
     self.weights = weights
 
 
+  def GetColHeight(self, col: np.array)->int:
+    """Returns the height of a column.
+    :param row: A 1-D array
+    :returns height of this column.
+    """
+    row_height = len(col)
+    for i in range(len(col)):
+      if col[i] != 0:
+        row_height = i
+        break
+    return len(col) - row_height
+
+  def GetHoles(self, game: game_client.GameClient) -> int:
+    total_cnt = 0
+    for c in range(game.map.shape[1]):
+      has_block = False
+      for r in range(game.map.shape[0]):
+        if game.map[r,c] != 0:
+          has_block = True
+
+        if game.map[r,c] == 0 and has_block:
+          total_cnt += 1
+    return total_cnt
 
   def MakeDecision(self) -> List[actions.Action]:
-    def GetColHeight(col: np.array)->int:
-      """Returns the height of a column.
-      :param row: A 1-D array
-      :returns height of this column.
-      """
-      row_height = len(col)
-      for i in range(len(col)):
-        if col[i] != 0:
-          row_height = i
-          break
-      return len(col) - row_height
-
-    def GetHoles(game: game_client.GameClient) -> int:
-      total_cnt = 0
-      for c in range(game.map.shape[1]):
-        has_block = False
-        for r in range(game.map.shape[0]):
-          if game.map[r,c] != 0:
-            has_block = True
-
-          if game.map[r,c] == 0 and has_block:
-            total_cnt += 1
-      return total_cnt
 
     def FindBestMove(ori_game):
+      if ori_game.CheckGameOver():
+        return (None, None)
+
       all_possible_solutions = agent.GetPossiblePositionsQuickVersion(
         state.current_piece, ori_game)
 
@@ -54,7 +59,6 @@ class TheNearPerfectAgent(agent.Agent):
 
       for move in all_possible_solutions:
         game = ori_game.copy()
-
         if move[1][0].swap:
           continue
 
@@ -63,10 +67,10 @@ class TheNearPerfectAgent(agent.Agent):
         game.PutPiece(move[0])
 
         # holes
-        holes = GetHoles(game)
+        holes = self.GetHoles(game)
 
         # Height of each col
-        col_heights = [GetColHeight(game.map[:, i]) for i in range(state.width)]
+        col_heights = [self.GetColHeight(game.map[:, i]) for i in range(state.width)]
 
         # aggregate_height
         aggregate_height = np.sum(col_heights)
@@ -84,7 +88,7 @@ class TheNearPerfectAgent(agent.Agent):
                        [aggregate_height, complete_lines, holes, bumpiness])
 
 
-        if score >= best_move_score:
+        if score > best_move_score:
           best_move = move
           best_move_score = score
       return (best_move_score, best_move)
@@ -93,4 +97,7 @@ class TheNearPerfectAgent(agent.Agent):
     ori_game = game_client.CreateGameFromState(state)
     (best_move_score, best_move) = FindBestMove(ori_game)
     print("line eliminated:", ori_game.accumulated_lines_eliminated)
-    return best_move[1]
+    if best_move:
+      return best_move[1]
+    else:
+      return [actions.Action()]

game_client.py
@@ -249,7 +249,7 @@ class GameClient(GameState):
         self.soft_drop = False
       else:
         if self.CheckValidity(self.current_piece, offset=(1, 0)):
-          self.Move(actions.Action(down=True, source_user=False))
+          self.Move(actions.Action(down=True, source_user_or_ai=False))
         else:
           if (not self._enable_lock_time or
               self.accumulate_lock_time >= self.current_maximum_lock_time):
@@ -272,11 +272,11 @@ class GameClient(GameState):
         break
       self.action_list.put(act)
 
-  def ProcessActions(self, action: List[actions.Action]):
-    for a in action:
-      self.ProcessAction(a)
+  def ProcessActions(self, actions: List[actions.Action], post_processing=True):
+    for a in actions:
+      self.ProcessAction(a, post_processing=post_processing)
 
-  def ProcessAction(self, action: actions.Action):
+  def ProcessAction(self, action: actions.Action, post_processing=True):
     if self.is_gameover:
       return
     # print(f"Processed action: {action.direction}, {action.rotation}, {action.swap}")
@@ -285,7 +285,7 @@ class GameClient(GameState):
     if action.swap:
       self.Swap()
     self.Rotate(action.rotation)
-    self.Move(action)
+    self.Move(action, post_processing=post_processing)
 
   def _ProcessActionsThread(self):
     while True:
@@ -308,9 +308,11 @@ class GameClient(GameState):
     self.level += inc
     self.SetLevel(self.level)
 
-  def Move(self, action: actions.Action) -> bool:
+  def Move(self, action: actions.Action, post_processing=True) -> bool:
     """Moves the current piece.
     :param direction: Direction to move
+    :param post_processing: if True, put the piece to map and
+           apply line eliminate. Otherwise just update the current_piece's states.
     :return True if moved; False otherwise
     """
     if (action.direction == actions.NONE and
@@ -345,7 +347,7 @@ class GameClient(GameState):
           moved = True
       finally:
         self.mutex_current_piece.release()
-    if action.direction == actions.HARD_DROP:
+    if action.direction == actions.HARD_DROP or action.direction == actions.SOFT_DROP :
       try:
         self.mutex_current_piece.acquire()
         while self.CheckValidity(self.current_piece, (1, 0)):
@@ -353,7 +355,8 @@ class GameClient(GameState):
           moved = True
       finally:
         self.mutex_current_piece.release()
-        self.PutPiece()
+        if post_processing and action.direction == actions.HARD_DROP :
+          self.PutPiece()
 
     if moved:
       self.last_action = action
@@ -559,50 +562,75 @@ class GameClient(GameState):
           return True
       return False
 
-    offset_map_jlstz = np.array([
+#
+#      WALLKICK_NORMAL_180[][][] =
+#      {
+#        {{1, 0}, {2, 0}, {1, 1}, {2, 1}, {-1, 0}, {-2, 0}, {-1, 1}, {-2, 1}, {0, -1}, {3, 0}, {-3, 0}}, // 0 >> 2─┐
+#      {{0, 1}, {0, 2}, {-1, 1}, {-1, 2}, {0, -1}, {0, -2}, {-1, -1}, {-1, -2}, {1, 0}, {0, 3}, {0, -3}}, // 1 >> 3─┼┐
+#      {{-1, 0}, {-2, 0}, {-1, -1}, {-2, -1}, {1, 0}, {2, 0}, {1, -1}, {2, -1}, {0, 1}, {-3, 0}, {3, 0}}, // 2 >> 0─┘│
+#      {{0, 1}, {0, 2}, {1, 1}, {1, 2}, {0, -1}, {0, -2}, {1, -1}, {1, -2}, {-1, 0}, {0, 3}, {0, -3}}, // 3 >> 1──┘
+#      };
+#      private
+#      static
+#      final
+#      int
+#      WALLKICK_I_180[][][] =
+#      {
+#        {{-1, 0}, {-2, 0}, {1, 0}, {2, 0}, {0, 1}}, // 0 >> 2─┐
+#      {{0, 1}, {0, 2}, {0, -1}, {0, -2}, {-1, 0}}, // 1 >> 3─┼┐
+#      {{1, 0}, {2, 0}, {-1, 0}, {-2, 0}, {0, -1}}, // 2 >> 0─┘│
+#      {{0, 1}, {0, 2}, {0, -1}, {0, -2}, {1, 0}}, // 3 >> 1──┘
+#      };
+#
+
+
+    # The 180 rotation wall kick table is copied from
+    # https://tetris.fandom.com/wiki/SRS#180.C2.B0_rotation
+    # which is origined from
+    # https://github.com/JoshuaWebb/nullpomino/blob/master/src/mu/nu/nullpo/game/subsystem/wallkick/StandardWallkick.java
+    offset_map_jlstz = [
       # state 0
-      [[(0, 0), (0, -1), (-1, -1), (2, 0), (2, -1)],  # 0>>1
-       [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],  # 0>>2, 180 rotation
-       [(0, 0), (0, 1), (-1, 1), (2, 0), (2, 1)]],  # 0>>3
+      ([(0, 0), (0, -1), (-1, -1), (2, 0), (2, -1)],  # 0>>1
+       [(1, 0), (2, 0), (1, 1), (2, 1), (-1, 0), (-2, 0), (-1, 1), (-2, 1), (0, -1), (3, 0), (-3, 0)],  # 0>>2, 180 rotation
+       [(0, 0), (0, 1), (-1, 1), (2, 0), (2, 1)]),  # 0>>3
 
       # state 1
-      [[(0, 0), (0, 1), (1, 1), (-2, 0), (-2, 1)],  # 1>>2
-       [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],  # l>>3, 180 rotation
-       [(0, 0), (0, 1), (1, 1), (-2, 0), (-2, 1)]],  # 1>>0
+      ([(0, 0), (0, 1), (1, 1), (-2, 0), (-2, 1)],  # 1>>2
+       [(0, 1), (0, 2), (-1, 1), (-1, 2), (0, -1), (0, -2), (-1, -1), (-1, -2), (1, 0), (0, 3), (0, -3)],  # l>>3, 180 rotation
+       [(0, 0), (0, 1), (1, 1), (-2, 0), (-2, 1)]),  # 1>>0
 
       # state 2
-      [[(0, 0), (0, 1), (-1, 1), (2, 0), (2, 1)],  # 2>>3
-       [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],  # 2>>0,
-       [(0, 0), (0, -1), (-1, -1), (2, 0), (2, -1)]],  # 2>>1
+      ([(0, 0), (0, 1), (-1, 1), (2, 0), (2, 1)],  # 2>>3
+       [(-1, 0), (-2, 0), (-1, -1), (-2, -1), (1, 0), (2, 0), (1, -1), (2, -1), (0, 1), (-3, 0), (3, 0)],  # 2>>0,
+       [(0, 0), (0, -1), (-1, -1), (2, 0), (2, -1)]),  # 2>>1
 
       # state 3
-      [[(0, 0), (0, -1), (1, -1), (2, 0), (-2, -1)],  # 3>>0
-       [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],  # 3>>1, 180 rotation
-       [(0, 0), (0, -1), (1, -1), (2, 0), (-2, -1)]],  # 3>>2
-    ])
+      ([(0, 0), (0, -1), (1, -1), (2, 0), (-2, -1)],  # 3>>0
+       [(0, 1), (0, 2), (1, 1), (1, 2), (0, -1), (0, -2), (1, -1), (1, -2), (-1, 0), (0, 3), (0, -3)],  # 3>>1, 180 rotation
+       [(0, 0), (0, -1), (1, -1), (2, 0), (-2, -1)]),  # 3>>2
+    ]
 
-    offset_map_i = np.array([
+    offset_map_i = [
       # state 0
       [[(0, 0), (0, -2), (0, 1), (1, -2), (-2, 1), ],  # 0>>1
-       [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],  # 0>>2, 180 rotation
+       [(-1, 0), (-2, 0), (1, 0), (2, 0), (0, 1)],  # 0>>2, 180 rotation
        [(0, 0), (0, -1), (0, 2), (-2, -1), (1, 2)]],  # 0>>3
 
       # state 1
       [[(0, 0), (0, -1), (0, 2), (-2, -1), (1, 2)],  # 1>>2
-       [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],  # 1>>3, 180 rotation,
+       [(0, 1), (0, 2), (0, -1), (0, -2), (-1, 0)],  # 1>>3, 180 rotation,
        [(0, 0), (0, 2), (0, -1), (-1, 2), (2, -1)]],  # 1>>0
 
       # state 2
       [[(0, 0), (0, 2), (0, -1), (-1, 2), (2, -1)],  # 2>>3
-       [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],  # 2>>0, 180 rotation
+       [(1, 0), (2, 0), (-1, 0), (-2, 0), (0, -1)],  # 2>>0, 180 rotation
        [(0, 0), (0, 1), (0, -2), (2, 1), (-1, -2)]],  # 2>>1
 
       # state 3
       [[(0, 0), (0, 1), (0, -2), (2, 1), (-1, -2)],  # 3>>0
-       [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],  # 3>>1, 180 rotation
+       [(0, 1), (0, 2), (0, -1), (0, -2), (1, 0)],  # 3>>1, 180 rotation
        [(0, 0), (0, -2), (0, 1), (1, -2), (2, 1)]],  # 3>>2
     ]
-    )
 
     state = piece.state
     num_90rotations %= 4
@@ -634,6 +662,10 @@ class GameClient(GameState):
     return None
 
   def Rotate(self, n: int) -> bool:
+    """Rotates the current piece.
+    :param n:  rotations, in range [0,4)
+    :return: True if the current piece can be rotated.  False otherwise.
+    """
     n %= 4
     if n == 0:
       return False

game_client_tests.py
@@ -126,11 +126,42 @@ class TestGameClient(unittest.TestCase):
     self.assertFalse(self.game.PutPiece(i))
 
   def test_RotationOK(self):
-    self.game = game_client.GameClient(width=4, height=5)
+    self.game = game_client.GameClient(width=4, height=6, height_buffer=0)
+    self.game.map = np.array(
+      [# 0  1  2  3
+        [0, 0, 0, 0, ],  # 0
+        [0, 0, 0, 0, ],  # 1
+        [0, 0, 0, 0, ],  # 2
+        [0, 0, 0, 0, ],  # 3
+        [0, 0, 0, 0, ],  # 4
+        [0, 0, 0, 0, ],  # 5
+      ]
+    )
+
     t = shape.T()
-    (t.x, t.y) = (6,1)
+    (t.x, t.y) = (4,0)
+    # Rotate 1
     self.game.SpawnPiece(t)
     self.assertTrue(self.game.Rotate(1))
+    expected = shape.T(3, -1)
+    expected.state = 1
+    self.assertEqual(self.game.current_piece, expected)
+
+    # Rotate 2
+    self.game.SpawnPiece(t)
+    self.assertTrue(self.game.Rotate(2))
+    expected = shape.T(3, 0)
+    expected.state = 2
+    self.assertEqual(self.game.current_piece, expected)
+
+    # Rotate 3
+    self.game.SpawnPiece(t)
+    self.assertTrue(self.game.Rotate(3))
+    expected = shape.T(3, 1)
+    expected.state = 3
+    print(self.game.current_piece)
+    print(expected)
+    self.assertEqual(self.game.current_piece, expected)
 
   def test_RotationFail(self):
     self.game = game_client.GameClient(width=3, height=4)

shape.py
@@ -11,53 +11,53 @@ import numpy as np
 
 # These offsets are stored as global variables to avoid mem copy.
 _SHAPES_I = np.array([
-  [(1,0), (1,1), (1,2), (1,3)],
-  [(0,2), (1,2), (2,2), (3,2)],
-  [(2,0), (2,1), (2,2), (2,3)],
-  [(0,1), (1,1), (2,1), (3,1)],
-  ], dtype=np.int)
+  [(1, 0), (1, 1), (1, 2), (1, 3)],
+  [(0, 2), (1, 2), (2, 2), (3, 2)],
+  [(2, 0), (2, 1), (2, 2), (2, 3)],
+  [(0, 1), (1, 1), (2, 1), (3, 1)],
+], dtype=np.int)
 
 _SHAPES_J = np.array([
-  [(0,0), (1,0), (1,1), (1,2)],
-  [(0,1), (1,1), (2,1), (0,2)],
-  [(1,0), (1,1), (1,2), (2,2)],
-  [(2,0), (0,1), (1,1), (2,1)],
-  ], dtype=np.int)
+  [(0, 0), (1, 0), (1, 1), (1, 2)],
+  [(0, 1), (1, 1), (2, 1), (0, 2)],
+  [(1, 0), (1, 1), (1, 2), (2, 2)],
+  [(2, 0), (0, 1), (1, 1), (2, 1)],
+], dtype=np.int)
 
 _SHAPES_L = np.array([
-  [(1,0), (1,1), (0,2), (1,2)],
-  [(0,1), (1,1), (2,1), (2,2)],
-  [(1,0), (1,1), (1,2), (2,0)],
-  [(0,0), (0,1), (1,1), (2,1)],
-  ], dtype=np.int)
+  [(1, 0), (1, 1), (0, 2), (1, 2)],
+  [(0, 1), (1, 1), (2, 1), (2, 2)],
+  [(1, 0), (1, 1), (1, 2), (2, 0)],
+  [(0, 0), (0, 1), (1, 1), (2, 1)],
+], dtype=np.int)
 
 _SHAPES_O = np.array([
-  [(0,1), (1,1), (0,2), (1,2)],
-  [(0,1), (1,1), (0,2), (1,2)],
-  [(0,1), (1,1), (0,2), (1,2)],
-  [(0,1), (1,1), (0,2), (1,2)],
-  ], dtype=np.int)
+  [(0, 1), (1, 1), (0, 2), (1, 2)],
+  [(0, 1), (1, 1), (0, 2), (1, 2)],
+  [(0, 1), (1, 1), (0, 2), (1, 2)],
+  [(0, 1), (1, 1), (0, 2), (1, 2)],
+], dtype=np.int)
 
 _SHAPES_S = np.array([
-  [(1,0), (0,1), (1,1), (0,2)],
-  [(0,1), (1,1), (1,2), (2,2)],
-  [(1,1), (1,2), (2,0), (2,1)],
-  [(0,0), (1,0), (1,1), (2,1)],
-  ], dtype=np.int)
+  [(1, 0), (0, 1), (1, 1), (0, 2)],
+  [(0, 1), (1, 1), (1, 2), (2, 2)],
+  [(1, 1), (1, 2), (2, 0), (2, 1)],
+  [(0, 0), (1, 0), (1, 1), (2, 1)],
+], dtype=np.int)
 
 _SHAPES_T = np.array([
-  [(0,1), (1,0), (1,1), (1,2)],
-  [(0,1), (1,1), (2,1), (1,2)],
-  [(1,0), (1,1), (1,2), (2,1)],
-  [(1,0), (0,1), (1,1), (2,1)],
-  ], dtype=np.int)
+  [(0, 1), (1, 0), (1, 1), (1, 2)],
+  [(0, 1), (1, 1), (2, 1), (1, 2)],
+  [(1, 0), (1, 1), (1, 2), (2, 1)],
+  [(1, 0), (0, 1), (1, 1), (2, 1)],
+], dtype=np.int)
 
 _SHAPES_Z = np.array([
-  [(0,0), (0,1), (1,1), (1,2)],
-  [(1,1), (2,1), (1,2), (0,2)],
-  [(1,0), (1,1), (2,1), (2,2)],
-  [(1,0), (0,1), (1,1), (2,0)],
-  ], dtype=np.int)
+  [(0, 0), (0, 1), (1, 1), (1, 2)],
+  [(1, 1), (2, 1), (1, 2), (0, 2)],
+  [(1, 0), (1, 1), (2, 1), (2, 2)],
+  [(1, 0), (0, 1), (1, 1), (2, 0)],
+], dtype=np.int)
 
 class Shape:
   def __init__(self, start_x: int = 1, start_y: int = 3):
@@ -71,6 +71,13 @@ class Shape:
     # 2: rotation180 from spawn state
     # 3: rotation270 from spawn state
     self.state = 0
+    # 1: I
+    # 2: J
+    # 3: L
+    # 4: O
+    # 5: S
+    # 6: T
+    # 7: Z
     self.id = 0
 
   @abc.abstractmethod
@@ -86,7 +93,7 @@ class Shape:
     self.state = (self.state + 3) % 4
 
   def Rotate(self, times: int):
-    for i in range(times%4):
+    for i in range(times % 4):
       self.Rotate90()
 
   @abc.abstractmethod
@@ -102,23 +109,31 @@ class Shape:
     if other is None:
       return False
 
-    return (self.x == other.x and
-            self.y == other.y and
-            self.state == other.state)
+    # O shape
+    if self.id == 4:
+      return (self.x == other.x and self.y == other.y)
+    else:
+      return (self.x == other.x and
+              self.y == other.y and
+              self.state == other.state)
 
   def __str__(self):
     ret = "\n".join([
       f"({self.x}, {self.y})",
       f"state:{self.state}", ""
     ])
-    display_area = np.zeros((4,4), dtype=np.int)
-    for (i,j) in self.GetShape():
-      display_area[i,j] = 1
+    display_area = np.zeros((4, 4), dtype=np.int)
+    for (i, j) in self.GetShape():
+      display_area[i, j] = 1
     ret += display_area.__str__()
     return ret
 
   def __hash__(self):
-    return hash(self.state) ^ hash(self.x) ^ hash(self.y)
+    # O shape: ignore the rotation
+    if self.id == 4:
+      return hash(self.x) ^ hash(self.y)
+    else:
+      return hash(self.state) ^ hash(self.x) ^ hash(self.y)
 
   def copy(self):
     return copy.copy(self)
@@ -192,4 +207,3 @@ class Z(Shape):
   def Init(self):
     super().Init()
     self.shape = _SHAPES_Z
-